7e4a3e1f46ec963fe7423a8edae8eea835389729
jaypark <pjhyun1990@gmail.com>
2023-09-15 11:19:20 +0900

    init project & local docker-compose setting

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.7.15"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("com.palantir.docker") version "0.35.0"
+
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
+
+    id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
+}
+
+group = "hanghae99.plus2.team3"
+version = "0.0.1"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
+
+    runtimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.rest-assured:rest-assured:4.5.1")
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "17"
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
+
+    named<Jar>("jar") {
+        enabled = false
+    }
+
+    ktlint {
+        verbose.set(true)
+    }
+}
+
+
+docker {
+    name = project.name + ":" + version
+    setDockerfile(file("Dockerfile"))
+    files(tasks.bootJar.get().outputs.files)
+    buildArgs(
+        mapOf(
+            "JAR_FILE" to tasks.bootJar.get().outputs.files.singleFile.name
+        )
+    )
+}
